051fabab2f4b0793da1286df96a9ff1d556fad90
KaengEE <yopuiqwer@naver.com>
2023-12-06 10:33:05 +0900

    새할일 추가

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../firebase/config";
 
 export default function TodoForm() {
-  const [newTodo, setNewTodo] = useState('');
+  const [newTodo, setNewTodo] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(newTodo);
+    //console.log(newTodo);
 
-    setNewTodo('');
+    //컬렉션
+    const ref = collection(db, "todos");
+    //db에 추가
+    await addDoc(ref, { title: newTodo });
+
+    setNewTodo("");
   };
 
   return (
