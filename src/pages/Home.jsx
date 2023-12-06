a847e3e558c74a14892b6c9d4248e16d8afde470
KaengEE <yopuiqwer@naver.com>
2023-12-06 09:30:28 +0900

    파일 정리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import TodoList from '../components/TodoList';
-import TodoForm from '../components/TodoForm';
+import { useState } from "react";
+import TodoList from "../components/TodoList";
+import TodoForm from "../components/TodoForm";
 
 export default function Home() {
   const [todos, setTodos] = useState([
-    { title: '아침운동', id: 1 },
-    { title: '제시간에 학원오기', id: 2 },
-    { title: '점심먹기', id: 3 },
-    { title: '복습하기', id: 4 }
+    { title: "아침운동", id: 1 },
+    { title: "제시간에 학원오기", id: 2 },
+    { title: "점심먹기", id: 3 },
+    { title: "복습하기", id: 4 },
   ]);
 
   return (
