b41a97f7bab209f34f1ae0a1904eb34ab9198a33
KaengEE <yopuiqwer@naver.com>
2023-12-06 14:23:07 +0900

    유저별로 할일 가져오기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
 import TodoList from "../components/TodoList";
 import TodoForm from "../components/TodoForm";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { db } from "../firebase/config";
+import { useAuthContext } from "../context/useAuthContext";
 
 export default function Home() {
   const [todos, setTodos] = useState(null);
-  console.log(todos);
+  const { user } = useAuthContext();
+  //console.log(todos);
 
   //시작할때 데이터 가져오기
   useEffect(() => {
-    const ref = collection(db, "todos");
+    //where절이 있을경우 query로 만들어줘야함
+    const q = query(collection(db, "todos"), where("uid", "==", user.uid));
 
     //todos 컬렉션에 모든 문서들 가져오기
-    const unsub = onSnapshot(ref, (snapshot) => {
+    const unsub = onSnapshot(q, (snapshot) => {
       let results = [];
       //성공시 snapshot.docs에 모든 doc를 넣음
       snapshot.docs.forEach((doc) => {
