67a763bd922c4743bd268dac5dbc626c1fde24bc
KaengEE <yopuiqwer@naver.com>
2023-12-06 12:16:03 +0900

    로그인/로그아웃시 네브바 설정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { auth } from "../firebase/config";
 import { useAuthContext } from "../context/useAuthContext";
 
 export default function Navbar() {
-  const { dispatch } = useAuthContext();
+  const { dispatch, user } = useAuthContext();
   //로그아웃 함수
   const logout = () => {
     signOut(auth)
@@ -25,13 +25,20 @@ export default function Navbar() {
         <li>
           <Link to="/">홈</Link>
         </li>
-        <li>
-          <Link to="/login">로그인</Link>
-        </li>
-        <li>
-          <Link to="/signup">회원가입</Link>
-        </li>
-        <li onClick={logout}>로그아웃</li>
+        {/* 유저가 없을때 로그인/회원가입 활성화 */}
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+            <li>
+              <Link to="/signup">회원가입</Link>
+            </li>
+          </>
+        )}
+
+        {/* 유저가 있을때 로그아웃 버튼 활성화 */}
+        {user && <li onClick={logout}>로그아웃</li>}
       </ul>
     </nav>
   );
