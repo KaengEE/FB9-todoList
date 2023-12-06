ec518ab14a535f3c8843dd3b8e572f3ae6588c77
KaengEE <yopuiqwer@naver.com>
2023-12-06 15:38:37 +0900

    모달 배경색 추가

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,41 +4,48 @@ import { db } from "../firebase/config";
 
 export const Modal = ({ setModalOpen, selectedTodo }) => {
   //수정데이터
-  const [updatedTitle, setUpdatedTitle] = useState("");
+  const [updatedTodo, setUpdatedTodo] = useState("");
 
   const closeModal = () => {
     setModalOpen(false);
   };
 
   const inputChange = (e) => {
-    setUpdatedTitle(e.target.value);
+    setUpdatedTodo(e.target.value);
   };
 
   const editDoc = async () => {
-    //console.log(`id: ${selectedTodo.id}, 제목: ${updatedTitle}`);
+    //console.log(`id: ${selectedTodo.id}, 제목: ${updatedTodo}`);
 
     //update
     const ref = doc(db, "todos", selectedTodo.id);
-    await updateDoc(ref, { title: updatedTitle });
+    await updateDoc(ref, { title: updatedTodo });
     //모달창 닫기
     closeModal();
   };
 
   return (
-    <div className="modal-container">
-      <button onClick={closeModal} className="modal-close">
-        X
-      </button>
-      {selectedTodo && (
-        <>
-          <h3>TODO Edit</h3>
-          <p>기존 TODO : {selectedTodo.title}</p>
-          <input type="text" value={updatedTitle} onChange={inputChange} />
-          <button onClick={editDoc} className="edit-button">
-            수정
-          </button>
-        </>
-      )}
+    <div className="modal-background">
+      <div className="modal-container">
+        <button onClick={closeModal} className="modal-close">
+          X
+        </button>
+        {selectedTodo && (
+          <>
+            <h3>TODO Edit</h3>
+            <p>기존 TODO : {selectedTodo.title}</p>
+            <input
+              required
+              type="text"
+              value={updatedTodo}
+              onChange={inputChange}
+            />
+            <button onClick={editDoc} className="edit-button">
+              수정
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
