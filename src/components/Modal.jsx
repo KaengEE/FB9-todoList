97ed53d406097b01216232ca3ae067d3b8ecc008
KaengEE <yopuiqwer@naver.com>
2023-12-06 16:24:31 +0900

    공백 입력 오류 추가

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,11 @@
 import { doc, updateDoc } from "firebase/firestore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../firebase/config";
 
 export const Modal = ({ setModalOpen, selectedTodo }) => {
   //수정데이터
   const [updatedTodo, setUpdatedTodo] = useState("");
+  const [error, setError] = useState(false);
 
   const closeModal = () => {
     setModalOpen(false);
@@ -14,12 +15,20 @@ export const Modal = ({ setModalOpen, selectedTodo }) => {
     setUpdatedTodo(e.target.value);
   };
 
+  //시작할때 기존 todo 넣어주기
+  useEffect(() => {
+    setUpdatedTodo(selectedTodo.title);
+  }, []);
+
   const editDoc = async () => {
     //console.log(`id: ${selectedTodo.id}, 제목: ${updatedTodo}`);
-
+    if (updatedTodo.trim() === "" || updatedTodo == null) {
+      setError(true);
+      return;
+    }
     //update
     const ref = doc(db, "todos", selectedTodo.id);
-    await updateDoc(ref, { title: updatedTodo });
+    await updateDoc(ref, { title: updatedTodo.trim() });
     //모달창 닫기
     closeModal();
   };
@@ -33,13 +42,13 @@ export const Modal = ({ setModalOpen, selectedTodo }) => {
         {selectedTodo && (
           <>
             <h3>TODO Edit</h3>
-            <p>기존 TODO : {selectedTodo.title}</p>
             <input
               required
               type="text"
               value={updatedTodo}
               onChange={inputChange}
             />
+            {error && <p className="error">내용을 입력해주세요!</p>}
             <button onClick={editDoc} className="edit-button">
               수정
             </button>
