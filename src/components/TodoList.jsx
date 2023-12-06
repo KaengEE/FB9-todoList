f7a1efd7b27a3aac21a05ec8a3e7b9cdc7b7fc7c
KaengEE <yopuiqwer@naver.com>
2023-12-06 10:54:23 +0900

    삭제 기능 및 삭제버튼 css 추가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,21 @@
-export default function TodoList({ todos }) {
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebase/config";
 
+export default function TodoList({ todos }) {
   const handleClick = async (id) => {
-    console.log(id);
+    const ref = doc(db, "todos", id);
+    await deleteDoc(ref);
+    //console.log(id);
   };
 
   return (
     <div className="todo-list">
       <ul>
-        {todos.map(todo => (
-          <li key={todo.id} onClick={() => handleClick(todo.id)}>{todo.title}</li>
+        {todos.map((todo) => (
+          <li key={todo.id} className="todo-item">
+            {todo.title}
+            <button onClick={() => handleClick(todo.id)}>X</button>
+          </li>
         ))}
       </ul>
     </div>
