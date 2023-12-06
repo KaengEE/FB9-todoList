e314de929f820d1a2cd0525c6cd54c0e447f151e
KaengEE <yopuiqwer@naver.com>
2023-12-06 12:35:42 +0900

    수정/삭제 아이콘 추가 및 변경

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,17 @@
 import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebase/config";
+import { FiEdit } from "react-icons/fi";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 export default function TodoList({ todos }) {
+  //삭제함수
   const handleClick = async (id) => {
     const ref = doc(db, "todos", id);
     await deleteDoc(ref);
     //console.log(id);
   };
+
+  //수정함수
 
   return (
     <div className="todo-list">
@@ -14,7 +19,13 @@ export default function TodoList({ todos }) {
         {todos.map((todo) => (
           <li key={todo.id} className="todo-item">
             {todo.title}
-            <button onClick={() => handleClick(todo.id)}>X</button>
+            {/* 수정아이콘 */}
+            <FiEdit className="edit" onClick={() => handleClick(todo.id)} />
+            {/* 삭제아이콘 */}
+            <RiDeleteBin6Line
+              className="dele"
+              onClick={() => handleClick(todo.id)}
+            />
           </li>
         ))}
       </ul>
